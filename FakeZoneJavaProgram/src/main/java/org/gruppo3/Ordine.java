8a5098e6b07d7d9445d0ae56e378143e6269a256
daniele <danielepopones@gmail.com>
2026-04-23 17:45:20 +0200

    aggiunte le variabili e il toString

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/Ordine.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/Ordine.java
@@ -1,4 +1,25 @@
 package org.gruppo3;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Ordine {
+    private LocalDate dataOrdine;
+    private ArrayList<DettagliOrdine> dettagliProdotti;
+    private double importTotale;
+    private Utente utente;
+    private int numeroOrdine;
+
+    public Ordine(LocalDate dataOrdine, double importTotale, Utente utente, int numeroOrdine){
+        this.dataOrdine = dataOrdine;
+        this.importTotale = importTotale;
+        this.utente = utente;
+        this.numeroOrdine = numeroOrdine;
+    }
+
+    public String toString(){
+        return "la data dell'ordine e': " + dataOrdine + "\n l'importo dell'ordine e': " + importTotale + "\n l'utente che ha fatto l'ordine e': " + utente + "\n il numero dell'ordine e': " + numeroOrdine;
+    }
+
+
 }
