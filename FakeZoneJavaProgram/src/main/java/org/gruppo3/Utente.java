99603bcc7338a909b6a198be79db0e7fe8616719
daniele <danielepopones@gmail.com>
2026-04-23 13:20:24 +0200

    creato il costruttore parametrico di utente

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/Utente.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/Utente.java
@@ -12,4 +12,16 @@ public class Utente {
     private String email;
     private String indirizzo;
     private String numeroTel;
+
+    private Utente(String pasword, String nome, String cognome, LocalDate dataNascita, char sesso, String codiceFiscale, String email, String indirizzo, String numeroTel){
+        this.pasword = pasword;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+        this.sesso = sesso;
+        this.codiceFiscale = codiceFiscale;
+        this.email = email;
+        this.indirizzo = indirizzo;
+        this.numeroTel = numeroTel;
+    }
 }
