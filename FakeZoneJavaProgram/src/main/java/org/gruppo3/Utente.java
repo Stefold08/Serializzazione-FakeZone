ca000f1152d35e351f9041759725884f316ff69a
daniele <danielepopones@gmail.com>
2026-04-23 13:12:16 +0200

    create le variabili locali

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/Utente.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/Utente.java
@@ -1,4 +1,15 @@
 package org.gruppo3;
 
+import java.time.LocalDate;
+
 public class Utente {
+    private String pasword;
+    private String nome;
+    private String cognome;
+    private LocalDate dataNascita;
+    private char sesso;
+    private String codiceFiscale;
+    private String email;
+    private String indirizzo;
+    private String numeroTel;
 }
