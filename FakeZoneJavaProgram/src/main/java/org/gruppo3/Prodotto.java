c71eb4e0b91600a62fb58570b74f7b34ed4622dd
daniele <danielepopones@gmail.com>
2026-04-23 22:32:06 +0200

    create le variabili locali e fatto il costruttore parametrico insieme al toString

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/Prodotto.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/Prodotto.java
@@ -1,4 +1,23 @@
 package org.gruppo3;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Prodotto {
+    private String codice;
+    private String descrizione;
+    private ArrayList<String> pathImmagini;
+    private double costo;
+
+    public Prodotto(String codice, String descrizione, double costo){
+        this.codice = codice;
+        this.descrizione = descrizione;
+        this.costo = costo;
+    }
+
+    public String toString(){
+        return "il codice del prodotto e': " + codice + "\n descrizzione del prodotto: " + descrizione + "\n il costo del prodotto e': " + costo;
+    }
+
+
 }
