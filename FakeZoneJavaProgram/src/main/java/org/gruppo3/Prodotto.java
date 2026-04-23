518bfae2099cdec9d4362ec7b1ff0aec3c964652
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-04-23 13:35:23 +0200

    Metodo toString() per stamapre tutti i dati della classe Prodotto.java

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/Prodotto.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/Prodotto.java
@@ -4,14 +4,28 @@ import java.util.ArrayList;
 
 public class Prodotto {
     private String codice;
+    private String nomeProdotto;
     private String descrizione;
     private double costo;
     private ArrayList<String> pathImmagini;
 
-    public Prodotto(String codice, String descirzione, double costo){
+    public Prodotto(String codice, String descirzione, String nomeProdotto, double costo){
         this.codice = codice;
         this.descrizione = descirzione;
         this.costo = costo;
+        this.nomeProdotto = nomeProdotto;
         pathImmagini = new ArrayList<>();
+    }
+
+    @Override
+    public String toString(){
+        String messaggio = null;
+        messaggio = "Codice prodotto: " + codice + "\nNome: " + nomeProdotto + "\nDescrizione:\n" + descrizione + "\nCosto: " + costo + "€" + "\nImmagini:\n";
+
+        for (int i = 0; i < pathImmagini.size(); i++){
+            messaggio += pathImmagini.get(i) + "\n";
+        }
+
+        return messaggio;
     }
 }
