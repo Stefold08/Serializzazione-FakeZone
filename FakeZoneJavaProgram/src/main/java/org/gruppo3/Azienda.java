b733c24a12e59ad4dd922f142b83f6f4c5d3ad6d
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-04-23 13:08:52 +0200

    Aggiunto costruttore

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/Azienda.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/Azienda.java
@@ -8,4 +8,10 @@ public class Azienda {
     private ArrayList<Utente> utenti;
     private ArrayList<Prodotto> prodotti;
     private ArrayList<Ordine> ordiniDelGiorno;
+
+    public Azienda(){
+        utenti = new ArrayList<>();
+        prodotti = new ArrayList<>();
+        ordiniDelGiorno = new ArrayList<>();
+    }
 }
