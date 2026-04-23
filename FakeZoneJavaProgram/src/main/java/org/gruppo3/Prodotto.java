861e84bcdcc73620fd7998b837667f8ba2772422
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-04-23 13:30:28 +0200

    Aggiunto costruttore per la classe Prodotto.java

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/Prodotto.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/Prodotto.java
@@ -7,4 +7,11 @@ public class Prodotto {
     private String descrizione;
     private double costo;
     private ArrayList<String> pathImmagini;
+
+    public Prodotto(String codice, String descirzione, double costo){
+        this.codice = codice;
+        this.descrizione = descirzione;
+        this.costo = costo;
+        pathImmagini = new ArrayList<>();
+    }
 }
