4dfd57a4495369cbdc5549a067e746d9326b2bab
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-04-23 13:28:17 +0200

    Aggiunti metodi nella classe Prodotto.java

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/Prodotto.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/Prodotto.java
@@ -1,4 +1,10 @@
 package org.gruppo3;
 
+import java.util.ArrayList;
+
 public class Prodotto {
+    private String codice;
+    private String descrizione;
+    private double costo;
+    private ArrayList<String> pathImmagini;
 }
