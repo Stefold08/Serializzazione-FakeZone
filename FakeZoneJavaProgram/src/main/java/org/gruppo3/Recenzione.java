38696532bf05ca73de2a5e0db02f2430a6d63a34
daniele <danielepopones@gmail.com>
2026-04-24 11:33:12 +0200

    aggiunti le variabili e il costruttore parametrico

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/Recenzione.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/Recenzione.java
@@ -2,4 +2,17 @@ package org.gruppo3;
 
 // DA IMPLEMENTARE + AVANTI!!!
 public class Recenzione {
+    private int stelle;
+    private LocalDate dataRecenzione;
+    private Utente utente;
+    private String descrizzione;
+
+    public Recenzione(int stelle, LocalDate dataRecenzione, Utente utente, String descrizzione){
+        this.stelle = stelle;
+        this.dataRecenzione = dataRecenzione;
+        this.utente = utente;
+        this.descrizzione = descrizzione;
+    }
+
+    
 }
