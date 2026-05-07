73de9ca4d76c56b131b6d7bb36f42302b4a92e57
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-05-07 13:06:37 +0200

    Inizializzato metodo `menu()`

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/MainProgram.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/MainProgram.java
@@ -1,7 +1,24 @@
 package org.gruppo3;
 
+import java.util.Scanner;
+
 public class MainProgram {
+
+    private static Azienda azienda;
+
     static void main(String[] args){
 
+    }
+
+    private int menu(){
+        int scelta = 0;
+        Scanner in = new Scanner(System.in);
+
+        try{
+            System.out.println("Menu");
+            System.out.println("1) Inserisci dati utente");
+            System.out.println("2) Visualizza dati utente");
+            System.out.println("3) Inserisci nuovo prodotto");
+        }
     }
 }
