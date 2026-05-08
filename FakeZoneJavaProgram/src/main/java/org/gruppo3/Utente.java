819f5085f64544540c2358c00cc4aab1b084efa1
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-05-08 10:30:02 +0200

    Corretto errore di battitura

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/Utente.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/Utente.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 
 public class Utente implements Serializable{
     private static final long serialVersionUID = 1;
-    private String pasword;
+    private String password;
     private String nome;
     private String cognome;
     private LocalDate dataNascita;
@@ -16,7 +16,7 @@ public class Utente implements Serializable{
     private String numeroTel;
 
     public Utente(String pasword, String nome, String cognome, LocalDate dataNascita, char sesso, String codiceFiscale, String email, String indirizzo, String numeroTel){
-        this.pasword = pasword;
+        this.password = pasword;
         this.nome = nome;
         this.cognome = cognome;
         this.dataNascita = dataNascita;
@@ -29,7 +29,7 @@ public class Utente implements Serializable{
 
     @Override
     public String toString(){
-        return "Dati utente: " + "\n nome: " + nome + "\n cognome: " + cognome + "\n codice fiscale: " + codiceFiscale + "n" + "\n sesso: " + sesso + "\n la password e': " + pasword + "\n email: " + email + "\n indirizzo di casa: " + indirizzo + "\n data di nascita: " + dataNascita + "\n numero di tel: " + numeroTel;
+        return "Dati utente: " + "\n nome: " + nome + "\n cognome: " + cognome + "\n codice fiscale: " + codiceFiscale + "n" + "\n sesso: " + sesso + "\n la password e': " + password + "\n email: " + email + "\n indirizzo di casa: " + indirizzo + "\n data di nascita: " + dataNascita + "\n numero di tel: " + numeroTel;
     }
 
     public String getCodiceFiscale(){
