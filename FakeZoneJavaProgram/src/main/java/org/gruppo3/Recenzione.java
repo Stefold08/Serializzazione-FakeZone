5cea6ec770cf97257c9b265d42f94ab2367c24ec
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-05-19 14:15:15 +0200

    Iniziata implementazione della recenzione: aggiunti getter e metodo `toString`

--- FakeZoneJavaProgram/src/main/java/org/gruppo3/Recenzione.java
+++ FakeZoneJavaProgram/src/main/java/org/gruppo3/Recenzione.java
@@ -2,19 +2,37 @@ package org.gruppo3;
 
 import java.time.LocalDate;
 
-// DA IMPLEMENTARE + AVANTI!!!
 public class Recenzione {
     private int stelle;
     private LocalDate dataRecenzione;
     private Utente utente;
-    private String descrizzione;
+    private String descrizione;
 
-    public Recenzione(int stelle, LocalDate dataRecenzione, Utente utente, String descrizzione){
+    public Recenzione(int stelle, LocalDate dataRecenzione, Utente utente, String descrizione){
         this.stelle = stelle;
         this.dataRecenzione = dataRecenzione;
         this.utente = utente;
-        this.descrizzione = descrizzione;
+        this.descrizione = descrizione;
     }
 
+    @Override
+    public String toString(){
+        return "Utente: " + utente.toString() + "\nData recensione: " + dataRecenzione + "\nValutazione: " + stelle + "\nCommento:\n" + descrizione;
+    }
 
+    public int getStelle(){
+        return stelle;
+    }
+
+    public LocalDate getDataRecenzione(){
+        return dataRecenzione;
+    }
+
+    public String getDescrizione(){
+        return descrizione;
+    }
+
+    public Utente getUtente(){
+        return utente;
+    }
 }
